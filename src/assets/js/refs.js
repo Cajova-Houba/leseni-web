c60b1ee69f881301810869297b5fba39b3290010
Zdenek Vales <zdenek.vales@yoso.fi>
2025-09-26 20:04:22 +0200

    Populate the gallery through JS

--- src/assets/js/refs.js
+++ src/assets/js/refs.js
@@ -1,5 +1,11 @@
+const IMAGE_MODAL_ID = "imageModal";
+const GALLERY_CONTAINER_ID = "galleryContainer";
+
+const GALLERY_IMAGE_COUNT = 16;
+const GALLERY_IMAGES_PER_ROW = 4;
+
 // init modal window in refs page
-const imageModal = document.getElementById("imageModal")
+const imageModal = document.getElementById(IMAGE_MODAL_ID)
 
 // add event listener
 if (imageModal) {
@@ -15,4 +21,30 @@ if (imageModal) {
         const modalImageElement = imageModal.querySelector("#imageModalDisplay")
         modalImageElement.src = imgSource
   })
+}
+
+// auto-populate galery
+const galleryContainer = document.getElementById(GALLERY_CONTAINER_ID)
+if (galleryContainer) {
+  const rowCount = Math.ceil((1.0 * GALLERY_IMAGE_COUNT) / GALLERY_IMAGES_PER_ROW);
+
+  let imageId = 1;
+  let galleryContainerHtml = "";
+  for (let rowId = 0; rowId < rowCount; rowId++) {
+    galleryContainerHtml += "<div class=\"row justify-content-center\">" ;
+    for (let rowImageId = 0; rowImageId < GALLERY_IMAGES_PER_ROW; rowImageId++) {
+        const imageName = imageId < 10 ? "0" + imageId + ".jpeg" : imageId + ".jpeg";
+
+        galleryContainerHtml += `
+          <div class="col-lg-3 col-md-6 pb-4">
+            <img src="images/gallery/${imageName}" class="img-thumbnail" data-bs-toggle="modal" data-bs-target="#imageModal">
+          </div>
+        `
+
+        imageId += 1;
+    }
+    galleryContainerHtml += "</div>";
+  }
+
+  galleryContainer.innerHTML = galleryContainerHtml;
 }
